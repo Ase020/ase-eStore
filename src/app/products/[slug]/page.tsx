91873ce40c91c350a2674b061c8b517fe4002aad
felixNyalenda <felixasejunior@gmail.com>
2024-05-23 15:08:54 +0300

    filter products options

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { AddToCart, CustomizeProduct, ProductImages } from "@/components";
 import { wixClientServer } from "@/lib/useWixClient";
 import { notFound } from "next/navigation";
 import React from "react";
+import DOMPurify from "isomorphic-dompurify";
 
 async function Product({ params }: { params: { slug: string } }) {
   const wixClient = await wixClientServer();
@@ -13,7 +14,7 @@ async function Product({ params }: { params: { slug: string } }) {
   if (!items[0]) notFound();
   const product = items[0];
 
-  console.log("Product: ", items[0]);
+  // console.log("Product: ", product);
 
   return (
     <main className="container-padding flex flex-col lg:flex-row gap-6 md:gap-12 lg:gap-16">
@@ -26,15 +27,20 @@ async function Product({ params }: { params: { slug: string } }) {
       <section className="w-full lg:w-1/2 flex flex-col gap-6">
         <h1 className="text-4xl font-medium">{product.name}</h1>
 
-        <p className="text-gray-500">{product.description}</p>
+        <div
+          className="text-gray-500"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(product.description as string),
+          }}
+        />
 
         <div className="h-0.5 bg-gray-100" />
 
         {product.price?.price === product.price?.discountedPrice ? (
           <div className="flex items-center gap-4">
-            <h3 className="text-xl text-gray-500 line-through">
-              {product.price?.formatted?.price}
-            </h3>
+            <h2 className="font-medium text-2xl">
+              {product.price?.formatted?.discountedPrice}
+            </h2>
           </div>
         ) : (
           <div className="flex items-center gap-4">
@@ -49,14 +55,25 @@ async function Product({ params }: { params: { slug: string } }) {
 
         <div className="h-0.5 bg-gray-100" />
 
-        <CustomizeProduct />
+        {product.variants && product.productOptions && (
+          <CustomizeProduct
+            productId={product._id as string}
+            productOptions={product.productOptions}
+            variants={product.variants}
+          />
+        )}
         <AddToCart />
         <div className="h-0.5 bg-gray-100" />
 
         {product.additionalInfoSections?.map((section) => (
           <div className="text-sm" key={section.title}>
             <h4 className="font-medium mb-4">{section.title}</h4>
-            <p className="">{section.description}</p>
+            <div
+              className=""
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(section.description as string),
+              }}
+            />
           </div>
         ))}
       </section>
--- src/components/CustomizeProduct.tsx
+++ src/components/CustomizeProduct.tsx
@@ -1,45 +1,103 @@
+"use client";
+
+import { products } from "@wix/stores";
 import React from "react";
 
-function CustomizeProduct() {
+type CustomizeProductProps = {
+  productId: string;
+  productOptions: products.ProductOption[];
+  variants: products.Variant[];
+};
+
+function CustomizeProduct({
+  productId,
+  productOptions,
+  variants,
+}: CustomizeProductProps) {
+  const [selectedOptions, setselectedOptions] = React.useState<{
+    [key: string]: string;
+  }>({});
+
+  const handleOptionsSelect = (optionType: string, choice: string) => {
+    setselectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+  };
+
+  const isVariantInStock = (choices: { [key: string]: string }) => {
+    return variants.some((variant) => {
+      const variantChoices = variant.choices;
+
+      if (!variantChoices) return false;
+
+      return (
+        Object.entries(choices).every(
+          ([key, value]) => variantChoices[key] === value
+        ) &&
+        variant.stock?.inStock &&
+        variant.stock.quantity &&
+        variant.stock.quantity > 0
+      );
+    });
+  };
+
   return (
     <div className="flex flex-col gap-6">
-      <h4 className="font-medium">Choose a color</h4>
-      <ul className="flex items-center gap-3">
-        <li className="size-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
-          <div className="absolute size-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-        </li>
+      {productOptions.map((option) => (
+        <div className="flex flex-col gap-4" key={option.name}>
+          <h4 className="font-medium">Choose a {option.name}</h4>
+          <ul className="flex items-center gap-3">
+            {option.choices?.map((choice) => {
+              const disabled = !isVariantInStock({
+                ...selectedOptions,
+                [option.name!]: choice.description as string,
+              });
 
-        <li className="size-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500" />
+              const selected =
+                selectedOptions[option.name!] === choice.description;
 
-        <li className="size-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-green-500">
-          <div className="absolute w-10 h-0.5 rounded-full bg-red-500 top-1/2 left-1/2 transform rotate-45 -translate-x-1/2 -translate-y-1/2" />
-        </li>
-      </ul>
+              const clickHandler = disabled
+                ? undefined
+                : () => handleOptionsSelect(option.name!, choice.description!);
 
-      <h4 className="font-medium">Choose a size</h4>
-      <ul className="flex items-center gap-3">
-        <button
-          type="button"
-          className="ring-1 ring-red-primary text-red-primary rounded-md py-1 px-4 text-sm"
-        >
-          Small
-        </button>
-
-        <button
-          type="button"
-          className="ring-1 ring-red-primary text-white bg-red-primary rounded-md py-1 px-4 text-sm"
-        >
-          Medium
-        </button>
-
-        <button
-          type="button"
-          disabled
-          className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm disabled:cursor-not-allowed"
-        >
-          Large
-        </button>
-      </ul>
+              return option.name === "Color" ? (
+                <li
+                  className="size-8 rounded-full ring-1 ring-gray-300 relative"
+                  role="button"
+                  style={{
+                    backgroundColor: choice.value,
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {selected && (
+                    <div className="absolute size-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                  {disabled && (
+                    <div className="absolute w-10 h-0.5 rounded-full bg-red-500 top-1/2 left-1/2 transform rotate-45 -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                </li>
+              ) : (
+                <button
+                  type="button"
+                  className="ring-1 ring-red-primary text-red-primary rounded-md py-1 px-4 text-sm disabled:cursor-not-allowed"
+                  disabled={disabled}
+                  style={{
+                    backgroundColor: selected
+                      ? "#f35c7a"
+                      : disabled
+                      ? "#FBCFE8"
+                      : "white",
+                    color: selected || disabled ? "white" : "#f35c7a",
+                    boxShadow: disabled ? "none" : "",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {choice.description}
+                </button>
+              );
+            })}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
